cf792b93076223cd5c48c8769acf5d035bb3af24
Juan Pablo S <emanuelsaavedravargasjuan@gmail.com>
2026-04-05 00:44:12 -0400

    pagina confirmacion
    se arreglo el mensaje de comprobacion, ahora se redirige a una pagina, se creo un hrml y css para la pagina de comprobacion, por ultimo se modifico el jbscrib

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 document.getElementById('dynamicButton').addEventListener('click', function() {
 
-    // 👇 VAN AQUÍ DENTRO
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const correo = document.getElementById('correo').value;
     const mensaje = document.getElementById('mensaje').value;
 
     const texto = document.getElementById('dynamicText');
-    const form = document.querySelector('.formulario');
 
     if (nombre === '' || apellido === '' || correo === '' || mensaje === '') {
         texto.textContent = '⚠️ Debes completar todos los campos';
@@ -15,12 +13,6 @@ document.getElementById('dynamicButton').addEventListener('click', function() {
         return;
     }
 
-    texto.textContent = '✅ Formulario enviado correctamente';
-    texto.style.color = 'green';
-
-    form.reset();
-
-    setTimeout(() => {
-        texto.textContent = '';
-    }, 3000);
+    // Redirige a la página de confirmación
+    window.location.href = 'confirmacion.html';
 });
