d934b3ea7ea9dd0002ae8a383fed1cd6a9861c98
Juan Pablo S <emanuelsaavedravargasjuan@gmail.com>
2026-04-06 14:34:18 -0400

    Cambios evaluacion
    Se agrego el campo telefono en el formulario contacto, ya estaba implementado la validacion de todos los campos deben estar completos, por lo cual solo se agrego el telefono.

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ document.getElementById('dynamicButton').addEventListener('click', function() {
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const correo = document.getElementById('correo').value;
+    const telefono = document.getElementById('telefono').value;  // ← nuevo
     const mensaje = document.getElementById('mensaje').value;
 
     const texto = document.getElementById('dynamicText');
 
-    if (nombre === '' || apellido === '' || correo === '' || mensaje === '') {
+    if (nombre === '' || apellido === '' || correo === '' || telefono === '' || mensaje === '') {  // ← agregado
         texto.textContent = '⚠️ Debes completar todos los campos';
         texto.style.color = 'red';
         return;
     }
 
-    // Redirige a la página de confirmación
     window.location.href = 'confirmacion.html';
 });
